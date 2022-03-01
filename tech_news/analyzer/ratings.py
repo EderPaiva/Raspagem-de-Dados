059b8eb14592acc96c1563f82dd44764831f413a
EderPaiva <edergomesdepaiva12@gmail.com>
2022-03-01 11:08:07 -0300

    atualizaçao do readme.md

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,42 @@
+from tech_news.database import find_news
+from operator import itemgetter
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    popularity = []
+    notice_list = find_news()
+    # print(notice_list)
+    for notice in notice_list:
+        notice["popularity"] = notice[
+            "shares_count"] + notice["comments_count"]
+        popularity.append(notice)
+
+    ordened_by_popularity = sorted(popularity, key=itemgetter(
+        "popularity", "title"), reverse=1)
+    top_five = ordened_by_popularity[0:5]
+    title_and_url = []
+    for notice in top_five:
+        title_and_url.append((notice["title"], notice["url"]))
+
+    return title_and_url
 
 
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+    all_categories = []
+    notice_list = find_news()
+    for notice in notice_list:
+        for category in notice['categories']:
+            all_categories.append(category)
+
+    category_ordened = sorted(all_categories)
+
+    if not category_ordened:
+        return []
+    if len(category_ordened) <= 5:
+        return category_ordened
+    else:
+        return category_ordened[0:5]
--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,80 @@
+from tech_news.database import find_news
+import datetime
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    notice_list = find_news()
+    # Find news traz tudo do banco, com isso!
+    # preciso apenas popular meu array \/ abaixo
+    title_url_list = []
+    # print(notice_list, title)
+
+    for notice in notice_list:
+        if notice["title"] == title or notice["title"].upper() == title:
+            title_url_list.append((notice["title"], notice["url"]))
+
+    return title_url_list
 
 
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    # date = datetime.date.fromisoformat(date)
+    notice_list = find_news()
+    # print(notice_list, date)
+    title_url_list = []
+
+    for notice in notice_list:
+        try:
+            # fromisoformat( date_string ) ¶
+            # Retorne um datecorrespondente a um date_string fornecido
+            # no formato YYYY-MM-DD:
+            # https://docs.python.org/3/library/datetime.html
+            datetime.date.fromisoformat(date)
+            # gambiarra cabulosa quiz
+            # fazer bonito fiz feito 0 é true -1 e false
+            # 31/01/2020 revisão de código
+            # se ele não encontrar a data vai no formato valido estora um erro
+            # apenas capturei o erro e emiti a mensagem
+            # mas a documentação
+            # indica que foi passada um data não transformavel
+            # erro começa na linha 35
+            if notice["timestamp"].find(date) == 0:
+                title_url_list.append((notice["title"], notice["url"]))
+        except ValueError:
+            raise ValueError("Data inválida")
+
+    return title_url_list
 
 
 # Requisito 8
 def search_by_source(source):
     """Seu código deve vir aqui"""
+    notice_list = find_news()
+    # print(notice_list, source)
+    title_url_list = []
+
+    for notice in notice_list:
+        for fonte in notice["sources"]:
+            if fonte.upper() == source.upper():
+                title_url_list.append(
+                    (notice["title"], notice["url"]))
+
+    return title_url_list
 
 
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    notice_list = find_news()
+    # print(notice_list, source)
+    title_url_list = []
+
+    for notice in notice_list:
+        for categ in notice["categories"]:
+            if categ.upper() == category.upper():
+                title_url_list.append((notice["title"], notice["url"]))
+
+    return title_url_list
